c8fda715c24e89c616d9bda9d2512e3d64e8fd24
Eduards Klavins <klavins.eduards@gmail.com>
2016-11-22 15:42:02 +0100

    Added point 5 and half of point 6...

---
+++ jscode.js
@@ -0,0 +1,4 @@
+var img = document.createElement("img");
+img.src = "http://internal.grazitti.com/clients/live_grazitti/wp-content/uploads/2016/05/Data-Exploration.jpg";
+var src = document.getElementById("listOfAuthors");
+src.appendChild(img);
